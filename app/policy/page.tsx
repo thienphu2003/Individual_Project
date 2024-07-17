b33de3e9397ca2e4ab3182e233f4b41fbcc83bcd
volamthienphu <phuvo@hitek.com.vn>
2024-07-17 16:09:46 +0700

    FEAT: add policy and privacy page

---
+++ app/policy/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const PolicyPage = () => {
+  return (
+    <div className="w-full h-full text-center flex items-center justify-center">
+      <p className="text-neutral-400">This is policy page</p>
+    </div>
+  );
+};
+
+export default PolicyPage;
---
+++ app/privacy/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const PrivacyPage = () => {
+  return (
+    <div className="w-full h-full text-center flex items-center justify-center">
+      <p className="text-neutral-400">This is privacy page</p>
+    </div>
+  );
+};
+
+export default PrivacyPage;
